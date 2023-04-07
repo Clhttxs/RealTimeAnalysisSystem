b0cd25b3cc223fb0e9c5fc561d69be71d9b567a2
程橹豪 <129243242+Clhttxs@users.noreply.github.com>
2023-04-07 12:18:30 +0800

    Update TopicConstant.java

--- RealTime_Common/src/main/java/Constant/TopicConstant.java
+++ RealTime_Common/src/main/java/Constant/TopicConstant.java
@@ -3,6 +3,7 @@ package Constant;
 //常量(static final修饰)一般声明在接口中。
 //        无需实例化对象，直接用类名.变量就可以获取常量。
 //        就需要把类设置为太监类(抽象类接口)
+//        github上修改一波
 
 public interface TopicConstant {
     String ORIGINAL_LOG = "base_log";
